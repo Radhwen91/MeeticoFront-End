bf8e83450faa40d4d79686bcd5d4d52248cf0abc
ziedlahbib <lahbibmedzied@gmail.com>
2022-04-27 21:24:52 +0100

    ajouter list des utilisateur en affich detail

--- src/app/pages/trip-management/image/image.component.ts
+++ src/app/pages/trip-management/image/image.component.ts
@@ -4,6 +4,13 @@ import {MatCardModule} from '@angular/material/card';
 import { TripService } from 'src/app/services/tripservices/trip.service';
 import { FileDB } from 'src/app/models/fileDB';
 import { Trip } from 'src/app/models/trip';
+import { ViewChild} from '@angular/core';;
+import {MatSort, SortDirection} from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatChipsModule } from '@angular/material/chips';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-image',
@@ -15,6 +22,13 @@ export class ImageComponent implements OnInit {
   id:Number;
   file:FileDB;
   trip: Trip;
+  users :User[];
+  counters = [100, 200, 10];
+  displayedColumns = ['lastName','firstName', 'email'];
+  dataSource: MatTableDataSource<User>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
   constructor(private tripservice:TripService,private router:ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -29,8 +43,18 @@ export class ImageComponent implements OnInit {
       res=>{
         console.log(res)
         this.trip=res;
+        this.users=this.trip.users
+        this.dataSource = new MatTableDataSource(this.users);
+        this.dataSource._renderChangesSubscription;
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
       }
     )
+  }
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
   }
   getimageurl(id:Number):String{
     var url="http://localhost:8089/SpringMVC/File/files/"+id;
